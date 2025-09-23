8920182eca64b054e8a70d269da95f84aafbc7c1
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-09-23 10:07:47 +0100

    push pour cloudflare

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,26 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://80fd3e01.cargameonline.pages.dev/' : undefined,
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'i18next', 'react-i18next'],
+    optimizeCss: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        ...config.optimization.splitChunks,
+        cacheGroups: {
+          ...config.optimization.splitChunks.cacheGroups,
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
