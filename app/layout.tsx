9ad388ebbe81667d1483a7d83d353dbd00526b85
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-09-23 11:45:54 +0100

    google teg

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,15 +83,17 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* ✅ Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-VYJ53LHQTQ"
+           src="https://www.googletagmanager.com/gtag/js?id=G-ZNELNXZ53K"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VYJ53LHQTQ');
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZNELNXZ53K');
+
           `}
         </Script>
 
