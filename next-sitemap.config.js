fdd84049a1f0151a2f26c76fa784d433c2e8966d
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-09-25 12:47:38 +0100

    site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://cargameonline.com',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-}
+  siteUrl: "https://cargameonline.com",
+  generateRobotsTxt: true, // Génère aussi robots.txt
+  sitemapSize: 5000,
+};
