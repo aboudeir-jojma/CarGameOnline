6557eb5533643de2dd82e999b01d9ebf42d35622
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-09-26 10:01:40 +0100

    site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,21 +2,9 @@
 const config = {
   siteUrl: "https://cargameonline.com",
   generateRobotsTxt: true,
+  generateIndexSitemap: true, // ⚡ IMPORTANT
   outDir: "./out",
   sitemapSize: 5000,
-
-  // On force la génération d’un index valide
-  generateIndexSitemap: true,
-
-  // Si tu veux inclure les pages dynamiques (jeux)
-  async additionalPaths(config) {
-    const games = require("./content/games.json"); // adapte si ton fichier est ailleurs
-    return games.map((game) => ({
-      loc: `/game/${game.slug}`,
-      changefreq: "weekly",
-      priority: 0.8,
-    }));
-  },
 };
 
 module.exports = config;
